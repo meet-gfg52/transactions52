f38f7d6d910cf442b9b0b250cd4ac1557d72b732
meet-gfg <Desire@4241>
2023-07-09 16:49:08 +0530

    Initial commit

---
+++ src/test/java/com/gfg/ewallet/Transactions52ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfg.ewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Transactions52ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
